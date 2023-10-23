c3489842c94a0b6b5db24d9b1be5e470d9ca343b
Alican Dağıdır <alicandagidir@hotmail.com>
2023-10-24 02:13:33 +0300

    Add project files.

---
+++ Ders 18/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ders_18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = 20;
+
+            switch (number)
+            {
+                case 10:
+                    Console.WriteLine("Number is 10");
+                    break;
+                case 20:
+                    Console.WriteLine("Number is 20");
+                    break;
+                default:
+                    Console.WriteLine("Number is not 10, nor 20");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
